f784aa3b8dd88a3466123bba44db36b2af7090bd
Lauren Hess <laurenbethhess@gmail.com>
2021-10-13 13:02:18 -0400

    commit tree 7d2fd31f3e42da82779d62d5c9b1dd367b52802b

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const dodger = document.getElementById("dodger"); 
-undefined
 function moveDodgerLeft() {
   const leftNumbers = dodger.style.left.replace("px", "");
   const left = parseInt(leftNumbers, 10);
@@ -8,13 +7,13 @@ function moveDodgerLeft() {
     dodger.style.left = `${left - 1}px`;
   }
 } 
-undefined
+
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft();
   }
 }); 
-undefined
+
 
 function moveDodgerRight() {
     const leftNumbers = dodger.style.left.replace("px", "");
@@ -24,10 +23,9 @@ function moveDodgerRight() {
       dodger.style.left = `${left + 1}px`;
     }
   } 
-  undefined
+ 
   document.addEventListener("keydown", function(e) {
     if (e.key === "ArrowRight") {
       moveDodgerRight();
     }
   }); 
-  undefined
